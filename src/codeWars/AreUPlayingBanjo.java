14e8eb53460febc964288a377c3e1042336bfc2b
Khai221 <juliocnarciso@hotmail.com>
2024-09-29 19:30:45 -0400

    first commit

---
+++ src/codeWars/AreUPlayingBanjo.java
@@ -0,0 +1,24 @@
+package codeWars;
+/*
+        Create a function which answers the question "Are you playing banjo?".
+        If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+        The function takes a name as its only argument, and returns one of the following strings:
+
+        name + " plays banjo"
+        name + " does not play banjo"
+        Names given are always valid strings.
+ */
+
+public class AreUPlayingBanjo {
+    public static String areYouPlayingBanjo(String name) {
+        // Program me!
+
+        if (name.toLowerCase().startsWith("r")) {
+            return name + " plays banjo";
+        } else {
+            return name + " does not play banjo";
+        }
+
+    }
+}
